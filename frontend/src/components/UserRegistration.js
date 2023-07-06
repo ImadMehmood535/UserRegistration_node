4183d64b99298658358f4aef0bf0d48d6572801c
ImadMehmood535 <imadmehmood816@gmail.com>
2023-07-06 18:07:47 +0500

    Showing server side response to client side

--- frontend/src/components/UserRegistration.js
+++ frontend/src/components/UserRegistration.js
@@ -1,75 +1,111 @@
 import React, { useState } from 'react';
-import axios from "axios"
-import "./UserRegistration.css"
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './UserRegistration.css';
 
 const UserRegistration = () => {
   const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: ""
-  })
+    name: '',
+    email: '',
+    password: ''
+  });
 
-  const handleChange = e => {
+  const [alertType, setAlertType] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    console.log(value);
     setUser({
-      ...user, [name]: value
-    })
-  }
+      ...user,
+      [name]: value
+    });
+  };
 
- 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(user)
+    console.log(user);
+  };
 
-  }
+  const register = () => {
+    axios
+      .post('http://localhost:4000/api/register', user)
+      .then((response) => {
+        const statusCode = response.status;
+        if (statusCode === 201) {
+          console.log(response);
+          setAlertType('success');
+          setAlertMessage(response.data.message);
+        }
+      })
+      .catch((err) => {
+        if (err.response && err.response.status === 400 && err.response.data.errors) {
+          const errorMessages = err.response.data.errors.map((err, index) => (
+            <div key={index}>{err.msg}</div>
+          ));
+          setAlertType('danger');
+          setAlertMessage(errorMessages);
+        } else {
+          setAlertType('danger');
+        }
+      });
 
-  const register = () =>{
-   
-    axios.post("http://localhost:4000/api/register", user)
-    .then((response) =>{
-        alert("User Registered")
-    }).catch((err) => alert("not created"))
-
-  }
-
-
+    // Clear user input after registration
+    setUser({
+      name: '',
+      email: '',
+      password: ''
+    });
+  };
 
   return (
-    <div className='register-user'>
-     <div  className='registration-box'>
-      <h1>Registration</h1>
-      <form onSubmit={handleSubmit}>
-        <label> Name:
-          <input
-            placeholder='Enter your name'
-            type='text'
-            name='name'
-            value={user.name}
-            onChange={handleChange} />
-        </label>
+<div>
+      {alertType && (
+        <div className={`alert alert-${alertType} text-center`} role="alert">
+          {alertMessage}
+        </div>
+      )}
+      <div className="register-user">
+        <div className="registration-box">
+          <h1>Registration</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
+              <input
+                placeholder="Enter your name"
+                type="text"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+              />
+            </label>
 
-        <label> Email:
-          <input
-            placeholder='Enter your email'
-            type='text'
-            name='email'
-            value={user.email}
-            onChange={handleChange} />
-        </label>
+            <label>
+              Email:
+              <input
+                placeholder="Enter your email"
+                type="text"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+              />
+            </label>
 
-        <label> Password:
-          <input
-            placeholder='Enter your password'
-            type='password'
-            name='password'
-            value={user.password}
-            onChange={handleChange} />
-        </label>
+            <label>
+              Password:
+              <input
+                placeholder="Enter your password"
+                type="password"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+              />
+            </label>
 
-        <button type="submit" onClick={register}>Register</button>
-
-      </form>
+            <button type="button" onClick={register}>
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
